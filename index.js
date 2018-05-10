0a5bc5c2803fc7457914dd2bd5b6aa241f348ea3
Steve Winberg <stevewin21@gmail.com>
2018-05-10 04:06:50 -0600

    add debug to module

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 (function() {
-  var mime, path, virtual;
+  var debug, mime, path, virtual;
 
   path = require('path');
 
   mime = require('mime');
 
+  debug = require('debug')('virtual-ext');
+
   virtual = function(req, res, next) {
     var accept, ext, regex, url;
+    debug('Executing virtual-ext middleware');
     accept = mime.getType(req.path);
     ext = path.extname(req.path);
     regex = new RegExp(`${ext}(\\?.+|)$`);
     url = req.url.replace(regex, '$1');
     if (accept && url !== req.url) {
+      debug('Update Accept header from %s to %s', req.headers.accept, accept);
       req.headers.accept = accept;
+      debug('Update URL from %s to %s', req.url, url);
       req.url = url;
     }
+    debug('Exiting virtual-ext middleware');
     return next();
   };
 
