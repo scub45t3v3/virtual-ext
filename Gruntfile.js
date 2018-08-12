8329a31fa9bf3d1dd2a26ef905eb4e4f129c2a34
Steve Winberg <stevewin21@gmail.com>
2018-08-12 15:56:02 +0700

    add grunt task to lint

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(() => {
+  const tasks = (grunt) => {
+    // load task configuration
+    grunt.initConfig(require('./grunt'));
+
+    // load grunt tasks
+    grunt.loadNpmTasks('grunt-eslint');
+
+    // register tasks
+    grunt.registerTask('default', ['eslint']);
+  }; // end grunt wrapper
+
+  // export as commonjs module
+  module.exports = tasks;
+})(); // end IIFE
---
+++ grunt/index.js
@@ -0,0 +1,3 @@
+(() => {
+  exports.eslint = require('./eslint');
+})(); // end IIFE
