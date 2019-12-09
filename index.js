14569a81f85e7da8377d98bfb59e6a44968729dd
Steve Winberg <stevewin21@gmail.com>
2019-12-09 16:43:42 +0700

    remove iife from index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const path = require('path');
-  const mime = require('mime');
-  const debug = require('debug')('@scuba-squad:virtual-ext');
+// include dependencies
+const path = require('path');
+const mime = require('mime');
+const debug = require('debug')('@scuba-squad:virtual-ext');
 
-  const virtual = (req, res, next) => {
-    debug('call:virtual(req, res, next)');
-    const accept = mime.getType(req.path);
-    const ext = path.extname(req.path);
-    const regex = new RegExp(`${ext}(\\?.+|)$`);
-    const url = req.url.replace(regex, '$1');
+const virtual = (req, res, next) => {
+  debug('call:virtual(req, res, next)');
+  const accept = mime.getType(req.path);
+  const ext = path.extname(req.path);
+  const regex = new RegExp(`${ext}(\\?.+|)$`);
+  const url = req.url.replace(regex, '$1');
 
-    if (accept && url !== req.url) {
-      debug('before:set:req.headers.accept = %s', req.headers.accept);
-      req.headers.accept = accept;
-      debug('set:req.headers.accept = %s', req.headers.accept);
+  if (accept && url !== req.url) {
+    debug('before:set:req.headers.accept = %s', req.headers.accept);
+    req.headers.accept = accept;
+    debug('set:req.headers.accept = %s', req.headers.accept);
 
-      debug('before:set:req.url = %s', req.url);
-      req.url = url;
-      debug('set:req.url = %s', req.url);
-    }
+    debug('before:set:req.url = %s', req.url);
+    req.url = url;
+    debug('set:req.url = %s', req.url);
+  }
 
-    return next();
-  }; // end virtual
+  return next();
+}; // end virtual
 
-  // export middleware as commonjs module
-  module.exports = virtual;
-})(); // end IIFE
+// export middleware as commonjs module
+module.exports = virtual;
