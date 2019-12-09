5f4c4386ba28cd5587dcabca0413af2ba3f2a733
Steve Winberg <stevewin21@gmail.com>
2019-12-09 16:43:32 +0700

    remove iife from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  const tasks = (grunt) => {
-    // load task configuration
-    grunt.initConfig(require('./grunt'));
+const tasks = (grunt) => {
+  // load task configuration
+  grunt.initConfig(require('./grunt'));
 
-    // load grunt tasks
-    grunt.loadNpmTasks('grunt-eslint');
+  // load grunt tasks
+  grunt.loadNpmTasks('grunt-eslint');
 
-    // register tasks
-    grunt.registerTask('default', ['eslint']);
-  }; // end grunt wrapper
+  // register tasks
+  grunt.registerTask('default', ['eslint']);
+}; // end grunt wrapper
 
-  // export as commonjs module
-  module.exports = tasks;
-})(); // end IIFE
+// export as commonjs module
+module.exports = tasks;
